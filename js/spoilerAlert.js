49fbb6cd7348cb10e1504d292ec8cd0c82dee9f3
Nayara Ribeiro Freire <nayfreireof@gmail.com>
2020-07-10 21:58:14 -0300

    As páginas do menu foram todas criadas

--- js/spoilerAlert.js
+++ js/spoilerAlert.js
@@ -1,1 +1,3 @@
-alert("A página a seguir contém spoilers da série. As imagens estão desfocadas para evitar o spoiler, caso deseje visualizá-las, passe o mouse por cima")
+function spoilerAlert(){
+    alert("A página a seguir contém spoilers da série. As imagens estão desfocadas para evitar o spoiler, caso deseje visualizá-las, passe o mouse por cima")
+}
