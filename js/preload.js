a673aeccd4ebce826eaac335cd8c006aac6c1881
Nayara Ribeiro Freire <nayfreireof@gmail.com>
2020-08-24 18:51:41 -0300

    Adicionado o formulário

--- js/preload.js
+++ js/preload.js
@@ -2,12 +2,12 @@ function aviso(){
     var r=confirm("A página a seguir contem um vídeo com luzes piscando em seu plano de fundo, caso deseje continuar, clique em 'OK', caso prefira outro vídeo, em 'Cancelar'");
     if (r==true){
         document.getElementById("bodyParte").innerHTML = '<video src="videos/supernatural-intros-seasons-1-15.mp4" autoplay muted loop id="conteudo"></video>'
-        document.getElementById("bodyParte").innerHTML += '<div id="login"> <div id="loginDiv"> <label for="User">Usuário</label> <br> <input type="text"> <br> <label for="password">Senha</label> <br> <input type="password"> <a href="paginas/index.html"><input id="btnEnvia" type="button" value="Entrar"></a> </div> </div>'
     }
     else{
         document.getElementById("bodyParte").innerHTML = '<video src="videos/supernaturalBaby.mp4" autoplay muted loop id="conteudo"></video>'
-        document.getElementById("bodyParte").innerHTML += '<div id="login"> <div id="loginDiv"> <label for="User">Usuário</label> <br> <input type="text"> <br> <label for="password">Senha</label> <br> <input type="password"> <a href="paginas/index.html"><input id="btnEnvia" type="button" value="Entrar"></a> </div> </div>'
     }
+
+    document.getElementById("bodyParte").innerHTML += '<div id="login"><div id="loginDiv">    <form><label for="User">Usuário</label><br><input type="text"><br><label for="password">Senha</label> <br> <input type="password"> <a href="paginas/index.html">   <input id="btnEnvia" type="button" value="Entrar"> </a> </form> </div> </div>'
 }
 
 function funcao1(){
